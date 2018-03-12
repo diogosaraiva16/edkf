ab35a2611a40b6b3f7d0fda6605f4021477269bb
Edgard Kozlowski <edgardkozlowski@descmac108.local>
2018-03-12 10:21:50 -0300

    Add inject global styled components

---
+++ src/styled-components/Global.js
@@ -0,0 +1,29 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+*, *:before, *:after {
+    -webkit-box-sizing: border-box;
+       -moz-box-sizing: border-box;
+            box-sizing: border-box;
+  }
+  
+  body {
+    -webkit-text-rendering: optimizeLegibility;
+    -moz-text-rendering: optimizeLegibility;
+    -ms-text-rendering: optimizeLegibility;
+    -o-text-rendering: optimizeLegibility;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    padding: 0;
+  }
+  
+  @font-face {
+      font-family: 'Helvetica';
+    src: url('/fonts/HelveticaLTStd-Bold.woff2') format('woff2'),
+    url('/fonts/HelveticaLTStd-Bold.woff') format('woff');
+      font-weight: bold;
+      font-style: normal;
+  }
+`
